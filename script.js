bed31a7d3930b76d832f05583fdf3d22934eaad7
Louis Van Aken <vanakenlouis@gmail.com>
2022-01-07 17:57:42 +0100

    Added comma feature

--- script.js
+++ script.js
@@ -4,45 +4,61 @@ let operator = undefined; /* user's inputed operation. is a function */
 
 const screen = document.querySelector('#display');
 const numberButtons = document.querySelectorAll('button#number');
-const clearButton = document.querySelector('#clear')
-const additionButton = document.querySelector('#addition')
-const substractionButton = document.querySelector('#substraction')
-const multiplicationButton = document.querySelector('#multiplication')
-const divisionButton = document.querySelector('#division')
-const equalButton = document.querySelector('#equal')
+const clearButton = document.querySelector('#clear');
+const additionButton = document.querySelector('#addition');
+const substractionButton = document.querySelector('#substraction');
+const multiplicationButton = document.querySelector('#multiplication');
+const divisionButton = document.querySelector('#division');
+const equalButton = document.querySelector('#equal');
+const commaButton = document.querySelector('#comma');
 
 function updateDisplay() {
-    screen.textContent = currentValue.join('');
+    /* limit number length to 8 */
+    let isDecimal = currentValue.includes('.');
+    let integerLength = (isDecimal) ? currentValue.slice(0, currentValue.indexOf('.')).length : currentValue.length;
+    let decimalLength = 8 - integerLength;
+    let number = parseFloat(parseFloat(currentValue.join('')).toFixed(decimalLength));
+
+    let display = number.toString();
+    display = display.replace('.', '<span class="comma">.</span>'); /* add class to comma so it takes less width */
+
+    screen.innerHTML = display;
+}
+
+function inputNumber(value) {
+    if (!operator) previousValue = [];
+
+    /* not more than 8 numbers and not multiple 0 at the beginning */
+    if (currentValue.length < 8 && !(value == 0 && currentValue.length == 1 && currentValue[0] == 0)) {
+        if (currentValue.length == 1 && currentValue[0] == 0) currentValue = []; /* clear 0 if it's the only number */
+        currentValue.push(value);
+        updateDisplay();
+    }
+}
+
+function inputComma() {
+    if(currentValue.length != 0 && currentValue.length < 8 && !currentValue.includes('.')) {
+        currentValue.push('.');
+        updateDisplay();
+    }
 }
 
 function clear() {
     currentValue = [0];
     previousValue = [];
     operator = undefined;
-    screen.textContent = '0';
-}
-
-function inputNumber(value) {
-    if(!operator) previousValue = [];
-
-    /* not more than 8 numbers and not multiple 0 at the beginning */
-    if (currentValue.length < 8 && !(value == 0 && currentValue.length == 1 && currentValue[0] == 0)) {
-        /* clear 0 if it's the only number */
-        if (currentValue.length == 1 && currentValue[0] == 0) currentValue = [];
-        currentValue.push(value);
-        updateDisplay();
-    }
+    updateDisplay();
 }
 
 function executeOperation() {
     if (operator) {
         let previousNumber = parseFloat(previousValue.join(''));
         let currentNumber = parseFloat(currentValue.join(''));
-        let result = operator(previousNumber, currentNumber);
+        let result = operator(previousNumber, currentNumber); /* execute function stored in operator */
         currentValue = result.toString().split('');
 
         updateDisplay();
-        operator = undefined;
+        operator = undefined; /* reset operator */
         previousValue = currentValue;
         currentValue = [];
     }
@@ -50,7 +66,7 @@ function executeOperation() {
 
 function newOperation(ope) {
     /* execute the pending operation if there is one */
-    if (previousValue.length != 0 && currentValue.length != 0) {
+    if (operator && currentValue.length != 0) {
         executeOperation();
     } else if (currentValue.length != 0) {
         previousValue = currentValue;
@@ -66,6 +82,7 @@ numberButtons.forEach((btn) => {
 
 clearButton.addEventListener('mousedown', clear);
 equalButton.addEventListener('mousedown', executeOperation);
+commaButton.addEventListener('mousedown', inputComma);
 
 additionButton.addEventListener('mousedown', () => newOperation((a, b) => a + b));
 substractionButton.addEventListener('mousedown', () => newOperation((a, b) => a - b));
